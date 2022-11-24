617117a69ae7a4cba53929b605c6f8cdecc13d07
Aarish khan <aarishkhan13111@gmail.com>
2022-11-24 19:08:23 +0530

    Shoping cart

---
+++ nine-shopingcartwithapi/src/components/BuyPage.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import CartItem from "./cartItem";
+
+import { random, commerce } from "faker";
+import { Container, Col, Row } from "reactstrap";
+
+// const apiKey = "Insert_your_key_here";
+// const url = "https://api.pexels.com/v1/search?query=laptop&per_page=6&page=1";
+
+const localurl = "https://mocki.io/v1/a75dcd86-a79b-4ce9-a9e5-32119fba2e3e";
+
+const BuyPage = ({ addInCart }) => {
+  const [product, setProduct] = useState([]);
+
+  // const fetchPhotos = async () =>{
+  //     const {data} = await Axios.get(url,{
+  //         headers: {
+  //             Authorization: apiKey
+  //         }
+  //     })
+  // }
+
+  const fetchPhotos = async () => {
+    const { data } = await Axios.get(localurl);
+
+    const { photos } = data;
+
+    const allProduct = photos.map((photos) => ({
+      smallImage: photos.src.medium,
+      tinyImage: photos.src.tiny,
+      productName: random.word(),
+      productPrice: commerce.price(),
+      id: random.uuid(),
+    }));
+
+    setProduct(allProduct);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, []);
+
+  return (
+    <Container fluid>
+      <h1 className="text-success text-center">BuyPage</h1>
+      <Row>
+        {product.map((product) => (
+          <Col md={4} key={product.id}>
+            <CartItem product={product} addInCart={addInCart} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default BuyPage;
---
+++ nine-shopingcartwithapi/src/components/Cart.js
@@ -0,0 +1,63 @@
+import React from "react";
+import {
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Button,
+  CardHeader,
+  Card,
+  CardBody,
+  CardFooter,
+  Col,
+  Row,
+} from "reactstrap";
+
+const Cart = (CartItem, buyNow, removeItem) => {
+  let amount = 0;
+
+  CartItem.forEach((item) => {
+    amount = parseFloat(amount) + parseFloat(item.productPrice);
+  });
+
+  return (
+    <Container fluid>
+      <h1 className="text-success">Your Cart</h1>
+      <ListGroup>
+        {CartItem.map((item) => (
+          <ListGroupItem key={item.id}>
+            <Row>
+              <Col>
+                <img height={80} src={item.tinyImage} alt=""></img>
+              </Col>
+              <Col className="text-center">
+                <div className="text-primary">{item.productName}</div>
+                <span>price:-{item.productPrice}</span>
+                <Button color="danger" onClick={() => removeItem(item)}>
+                  Remove
+                </Button>
+              </Col>
+            </Row>
+          </ListGroupItem>
+        ))}
+      </ListGroup>
+      {/* if every thing is empty */}
+      {CartItem.length >= 1 ? (
+        <Card className="text-Center mt-3">
+          <CardHeader>GrandTotal</CardHeader>
+          <CardBody>
+            Your amount for {CartItem.length} product is {amount}
+          </CardBody>
+          <CardFooter>
+            <Button color="success" onClick={buyNow}>
+              Pay here
+            </Button>
+          </CardFooter>
+        </Card>
+      ) : (
+        <h1 className="text-warning">Cart is empty</h1>
+      )}
+    </Container>
+  );
+};
+
+export default Cart;
