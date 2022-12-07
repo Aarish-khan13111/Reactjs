903ce308678770b27ca2ebf62d51f94962de3199
Aarish khan <aarishkhan13111@gmail.com>
2022-12-07 15:38:47 +0530

    app with real time data base

---
+++ eleven-firebase-realtime-database/src/Utils/config.js
@@ -0,0 +1,18 @@
+//TODO: add firebase configuration and export it
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyCB7XjnZHm5s19y3eQZ3rRHUquBhbdaFKI",
+  authDomain: "mygitapp-797b5.firebaseapp.com",
+  projectId: "mygitapp-797b5",
+  storageBucket: "mygitapp-797b5.appspot.com",
+  messagingSenderId: "1075763093592",
+  appId: "1:1075763093592:web:e97a4f8ef0163ef13e2a2d",
+  measurementId: "G-8NZS9FLRVW",
+};
+//image configuration
+export const image_Congfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true,
+};
---
+++ eleven-firebase-realtime-database/src/layout/Footer.js
@@ -0,0 +1,13 @@
+//TODO: Export the Footer
+import React from "react";
+import { Container } from "reactstrap";
+
+const Footer = () => {
+  return (
+    <Container className="bg-dark text-white text-center" fluid>
+      A simple Contact App
+    </Container>
+  );
+};
+
+export default Footer;
